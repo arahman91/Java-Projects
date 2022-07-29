aa1b193be3b5e3f4070eec19b5c9223ad5a2ba15
arahman91 <arahman56@gmail.com>
2022-07-28 23:03:50 -0400

    Update CalculateMortgage.java
    Added the ability to show mortgage amount by path.

--- ITECodes/src/myCodes/CalculateMortgage.java
+++ ITECodes/src/myCodes/CalculateMortgage.java
@@ -1,4 +1,13 @@
-package myCodes;
+/*
+ * Takes the mortgage amount, interest and
+ * amortization period in years, and shows
+ * the monthly payment.
+ * 
+ * Can also show the amount left after each month.
+ * 
+ * @author arahman91
+ */
+
 
 import java.util.Scanner;
 
@@ -6,23 +15,32 @@ public class CalculateMortgage {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		boolean debug = false; //Show debug outputs
+		boolean showmonthly = true; //Show the monthly balance
 		
 		System.out.print("Enter the mortgage amount: ");
 		int mortgage = scan.nextInt();
 		System.out.print("Enter the annual interest percent: ");
-		double interest  = scan.nextInt(); 
-		interest = interest / (100*12);
+		double interest  = scan.nextDouble() / (100*12); 
+		//interest = interest / (100*12);
 		System.out.print("Enter the amortization in years: ");
 		int amort = scan.nextInt()*12;
 		scan.close();
 		
 		if (debug) {
 			System.out.printf("The amortization in months is: %d%n", amort);
-			 System.out.printf("The interest monthly is: %f%n" , interest);
+			System.out.printf("The interest monthly is: %f%n" , interest);
 		}
 		
 		double payment = (mortgage * interest)/(1 - (1 / Math.pow(1+interest, amort) ) );
-		System.out.printf("The monthly payment is: $%.2f" , payment); //Format the output to 2 decimals
+		System.out.printf("The monthly payment is: $%.2f%n" , payment); //Format the output to 2 decimals
+		
+		if (showmonthly) {
+			double principal = mortgage;
+			for (int i = 1; i <= amort; i++) {
+				principal = principal * (1 + interest) - payment;
+				System.out.printf("The principal at month %d is: $%.2f%n" , i, principal);
+			}
+		}
 	}
 
 }
